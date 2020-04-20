7f709e9947a74c29c98158019466332526f00455
swordfish291 <swordfish291@gmail.com>
2020-04-20 19:08:41 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<?php
+    $hostName = `hostname`;
+    echo "Instace Name: " . $hostName;
+
+if(array_key_exists('startStress', $_POST)){
+    startStress();
+}
+else if(array_key_exists('stopStress', $_POST)){
+    stopStress();
+}
+function startStress(){
+    `stress --cpu 4`;
+}
+function stopStress(){
+    `pkill stress`;
+}
+?>
+<form method="POST">
+    <input type="submit" name="startStress" class="button" value="startStress">
+    <input type="submit" name="stopStress" class="button" value="stopStress">
+</form>
+</html>
